87bc1729ee854276f2e774743d2c50b3d4f39028
adriantizocmarshall <161913596+adriantizocmarshall@users.noreply.github.com>
2026-03-07 15:36:50 -0500

    Add project files.

---
+++ BranchingSubmissionAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Planet Express! Please follow the instructions below.");
+            Console.ReadLine(); // Additional ReadLine() statements throughout for cleanliness of console
+            Console.WriteLine("Please enter the weight of your package in pounds.");
+            string weight = Console.ReadLine(); // Stores weight as a string for later integer conversion
+            Console.ReadLine();
+            if (Convert.ToInt32(weight) <= 50) // Checks if entered weight is less than or equal to 50 pounds
+                // If true, asks for package dimensions
+            {
+                Console.WriteLine("Please enter the package width in inches.");
+                string width = Console.ReadLine();
+                Console.ReadLine();
+                Console.WriteLine("Please enter the package height in inches.");
+                string height = Console.ReadLine();
+                Console.ReadLine();
+                Console.WriteLine("Please enter the package length in inches.");
+                string length = Console.ReadLine();
+                Console.ReadLine();
+                int dimensions = Convert.ToInt32(length) + Convert.ToInt32(width) + Convert.ToInt32(height);
+                if (dimensions >= 50) // Checks if dimensions are greater than or equal to 50
+                {
+                    // If true, declines user and terminates program
+                    Console.WriteLine("Package too big to be shipped via Planet Express. Have a nice day!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    // If false, calculates quote by multiplying dimensions and weight, then dividing by 100 to get final quote
+                    int total = Convert.ToInt32(length) * Convert.ToInt32(width) * Convert.ToInt32(height);
+                    int quote = total * Convert.ToInt32(weight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+                    Console.ReadLine();
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+            }
+            else if (Convert.ToInt32(weight) >= 50)
+            {
+                // If package weight is greater than or equal to 50 pounds, declines user
+                Console.WriteLine("Your package is too heavy to be shipped via Planet Express. Have a nice day!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BranchingSubmissionAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BranchingSubmissionAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BranchingSubmissionAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57328647-0ac2-4a14-a37c-5b8f26489b4a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
